4ed7b14869566ba78b30b9af93aad07bc021840c
unknown <fchepkoech76@gmail.com>
2024-10-02 14:37:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+randomNumber = Math.floor(Math.random()*8);
+eightBall ="";
+switch (randomNumber){
+  case 0:
+  eightBall ="It is certain";
+  break;
+   case 1:
+   eightBall ="It is decidedly so";
+  break;
+   case 2:
+   eightBall ="Reply hazy try again";
+  break;
+   case 3 :
+   eightBall ="cannot predict now";
+  break;
+   case 4 :
+   eightBall ="Do not count on it";
+  break;
+   case 5:
+   eightBall ="My sources say no";
+  break;
+   case 6 :
+   eightBall ="Outlook not so good";
+  break;
+   case 7 :
+  eightBall = "Signs point to yes";
+  break;
+   
+
+   
+
+}
+  console.log(eightBall);
